7ac281f13d775bf68f0912c055619dc0791e4ec4
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-11-25 19:35:58 +0100

    add urls

---
+++ src/app/shared/constants/urls.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://localhost:4725';
+
+export const FOODS_URL = BASE_URL + '/api/foods';
+export const FOODS_TAGS_URL = BASE_URL + '/api/foods/tags';
+export const FOODS_BY_SEARCH_URL = BASE_URL + '/api/foods/search/';
+export const FOODS_BY_TAG_URL = BASE_URL + '/api/foods/tag/';
+export const FOOD_BY_ID = BASE_URL + '/api/foods/';
