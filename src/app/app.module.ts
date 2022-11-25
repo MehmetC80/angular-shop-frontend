846508c1dc2a1a3a99cd6382f177ae2ade4c4515
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-11-25 20:42:19 +0100

    update app.modules create login-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,13 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
     NotFoundComponent,
     LoginPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RatingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RatingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/pages/login-page/login-page.component.ts
+++ src/app/components/pages/login-page/login-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitted = false;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+  submit() {
+    this.isSubmitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    } else {
+      alert(
+        `email: ${this.loginForm.controls.email.value}, password: ${this.loginForm.controls.password.value}`
+      );
+    }
+  }
 }
